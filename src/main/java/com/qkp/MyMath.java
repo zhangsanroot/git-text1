5eada14c0dad30c626fc08b0c8abc18200bf9567
zhangsan <zhangsan@itcast.com>
2021-11-16 08:26:21 +0800

    将本地工程添加到git

--- src/main/java/com/qkp/MyMath.java
+++ src/main/java/com/qkp/MyMath.java
@@ -2,13 +2,16 @@ package com.qkp;
 
 public class MyMath {
     public static int add(int a,int b){
+//        返回a+b
         return a+b;
     }
     public static int sub(int a,int b){
+//        返回a-b
         return a-b;
     }
     public void test(){
         int i=0;
         i +=10;
+        System.out.println(i);
     }
 }
